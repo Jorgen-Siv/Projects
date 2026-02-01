8337750e7722e7a79d7e5103757476ba0f4cb7eb
Jorgen-Siv <joergen.sivertsvik@gmail.com>
2026-02-01 22:18:44 +0100

    Update and rename main.cpp to Day1.cpp

--- AdventOfCode/Day1.cpp
+++ AdventOfCode/Day1.cpp
@@ -1,6 +1,3 @@
-// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string_view>
 #include <string>
@@ -103,7 +100,6 @@ int main() {
 	int start = 50;// start position of dial
 	std::ifstream inf{ "Text.txt" };
 
-	// If we couldn't open the output file stream for reading
 	if (!inf)
 	{
 		// Print an error and exit
@@ -124,13 +120,4 @@ int main() {
 	
 	return 0;
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
